79524207dba9138b47a0e637e8da483202f05c52
Derrick <dcheng04@mail.ubc.ca>
2022-04-01 15:27:32 -0700

    Small edit to background fetch

--- HealthPlatformWatchOS/AppDelegate.swift
+++ HealthPlatformWatchOS/AppDelegate.swift
@@ -13,16 +13,13 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     
     //Set the minimum background fetch interval in seconds. This interval is not an exact time interval of when each background fetch will be run. Setting the interval states that a background fetch will happen AT MOST once per X seconds, Apple determines the actual runtimes.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        application.setMinimumBackgroundFetchInterval(3600)
+        application.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
         return true
     }
 
     //Execute HealthKit Query and send to IoT synchonously on the background thread after verifying there is a connection to IoT. If this cannot be completed within the seconds specified by connectionTimeoutLimit, the background fetch will fail.
     func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         DispatchQueue.global(qos: .background).sync {
-            if (self.healthDataManager.connectionStatus != "Connected") {
-                self.healthDataManager.mqttClient = AWSViewModel()
-            }
             var connectionTimeoutCount = 0
             let connectionTimeoutLimit = 25 //Hard limit for a background fetch is 30s
             Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
@@ -32,9 +29,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
                     self.healthDataManager.queryHRVData()
                     completionHandler(.newData)
                     timer.invalidate()
-                } else if (connectionTimeoutCount == connectionTimeoutLimit) {
-                    self.healthDataManager.yes()
+                } else if connectionTimeoutCount == connectionTimeoutLimit {
                     completionHandler(.failed)
+                    timer.invalidate()
                 }
             }
         }
